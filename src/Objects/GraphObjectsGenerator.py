d52d6abb8f1f04fbccd8e969ce959ebeb967293f
SassonNir <nirsasson00@gmail.com>
2021-12-27 12:36:51 +0200

    Refactored generator

--- src/Objects/GraphObjectsGenerator.py
+++ src/Objects/GraphObjectsGenerator.py
@@ -19,36 +19,28 @@ class GraphObjectsGenerator:
         return [Node(i, random.uniform(0, 10), random.uniform(0, 10), random.uniform(0, 10)) for i in range(count)]
 
     def generate_empty_nodes(self, count: int):
-        nodes = []
-        for i in range(count):
-            nodes.append((None, None, None))
-        return nodes
+        return [(None, None, None)] * count
 
     def generate_graph(self, nodecount: int, avgdegree: int):
         nodes = self.generate_nodes(nodecount)
         edges = set()
-        indegree = []
-        outdegree = []
-        for i in range(nodecount):
-            inrank = random.randint(2, 5)
-            outrank = 10 - inrank
-            indegree.append(inrank)
-            outdegree.append(outrank)
+        indegree = [random.randint(2, 5) for _ in range(nodecount)]
+        outdegree = [10 - i for i in indegree]
         flag = False
         while not flag:
             src = random.randint(0, nodecount - 1)
             dest = random.randint(0, nodecount - 1)
             failedsrc = set()
-            while outdegree[src] == 0 and len(failedsrc) <= nodecount - 1:
+            while (outdegree[src] == 0) and (len(failedsrc) <= nodecount - 1):
                 failedsrc.add(src)
                 src = random.randint(0, nodecount - 1)
                 if src in failedsrc:
                     continue
             faileddest = set()
-            while indegree[dest] == 0 and len(faileddest) <= nodecount - 1:
+            while (indegree[dest] == 0) and (len(faileddest) <= nodecount - 1):
                 faileddest.add(dest)
                 dest = random.randint(0, nodecount - 1)
-                if dest in faileddest or src == dest:
+                if (dest in faileddest) or (src == dest):
                     continue
                 continue
             e = Edge(src, dest, nodes[src].distance(nodes[dest]))
@@ -67,28 +59,23 @@ class GraphObjectsGenerator:
     def generate_large_graph(self, nodecount: int):
         nodes = self.generate_nodes(nodecount)
         edges = set()
-        indegree = []
-        outdegree = []
-        for i in range(nodecount):
-            inrank = random.randint(4, 10)
-            outrank = 20 - inrank
-            indegree.append(inrank)
-            outdegree.append(outrank)
+        indegree = [random.randint(4, 10) for _ in range(nodecount)]
+        outdegree = [20 - i for i in indegree]
         flag = False
         while not flag:
             src = random.randint(0, nodecount - 1)
             dest = random.randint(0, nodecount - 1)
             failedsrc = set()
-            while outdegree[src] == 0 and len(failedsrc) <= nodecount - 1:
+            while (outdegree[src] == 0) and (len(failedsrc) <= nodecount - 1):
                 failedsrc.add(src)
                 src = random.randint(0, nodecount - 1)
                 if src in failedsrc:
                     continue
             faileddest = set()
-            while indegree[dest] == 0 and len(faileddest) <= nodecount - 1:
+            while (indegree[dest] == 0) and (len(faileddest) <= nodecount - 1):
                 faileddest.add(dest)
                 dest = random.randint(0, nodecount - 1)
-                if dest in faileddest or src == dest:
+                if (dest in faileddest) or (src == dest):
                     continue
                 continue
             e = Edge(src, dest, nodes[src].distance(nodes[dest]))
