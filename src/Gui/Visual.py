cf3d3e36185e8712f61e79e8dc70753fad6a840e
Ofri Tavor <73499329+Unusual55@users.noreply.github.com>
2021-12-28 17:51:59 +0200

    Update Visual.py

--- src/Gui/Visual.py
+++ src/Gui/Visual.py
@@ -12,6 +12,15 @@ from Objects.DiGraph import DiGraph
 from Objects.GraphAlgo import GraphAlgo
 
 
+""" This class is the graph visualiser. In this class we create a Directed Graph visualisation by creating matplotlib visual objects as our nodes and edges.
+Node will turn into scatter and Edge will turn into annotate of an arrow. This class handle every event of the graph, include addition of nodes or edges, or removing nodes or 
+edges as well as running and draw the algorithms the user will ask it to.
+There are 3 special features in our GUI, which we implemented using labels for the scatters and set them as pickable:
+1. When the user will click on right click, it will create a new Node and add it to the graph.
+2. When the user will double click on a Node, an event occur, and it will remove the Node from the graph.
+3. When the user will left click on a Node and the left click another Node, a popup will open, and if the user entered a positive real number, an event will trigger and add an 
+edge between the two nodes."""
+
 class Visual:
     # plt.tight_layout()
     counter = 0
@@ -33,6 +42,8 @@ class Visual:
         # self.ax.relim()
         # self.ax.autoscale()
 
+    """ This function handles the node addition event """    
+        
     def on_press(self, event):
 
         print("mouse clicked: x: " + str(event.xdata) + " y: " + str(event.ydata) + "button: " + str(event.button))
@@ -40,6 +51,8 @@ class Visual:
             key = self.add_node_by_click()
             self.add_node(key, (event.xdata, event.ydata, 0.0))
 
+    """ This function handles both node remove event and add edge event """        
+            
     def onpick(self, event):
         label = str(event.artist.get_label())
         if label.isalnum():
@@ -61,6 +74,8 @@ class Visual:
                 self.add_edge(self.edgestack[0], self.edgestack[1], w)
                 self.edgestack = []
 
+    """ This function set the bounding box of the plot """            
+                
     def set_bbox(self):
         minx, maxx, miny, maxy = float('inf'), float('-inf'), float('inf'), float('-inf')
         toupdate = []
@@ -78,12 +93,16 @@ class Visual:
         for do in toupdate:
             self.g.nodes.get(do, Node).setlimitedrandompos(self.xmin, self.ymin, 0.0, self.xmax, self.ymax, 0.0)
 
+    """ This function update the bounding box of the plot in case we added new node to the graph"""        
+            
     def bbox_update(self, n: Node):
         if not n.checkpos():
             return
         self.xmin, self.xmax = min(self.xmin, n.get_x()), max(self.xmax, n.get_x())
         self.ymin, self.ymax = min(self.ymin, n.get_y()), max(self.ymax, n.get_y())
 
+    """ This function draw the graph"""    
+        
     def draw_graph(self):
         self.ax.clear()
         for src, node in self.g.nodes.items():
@@ -98,6 +117,8 @@ class Visual:
         self.fig.canvas.draw()
         print(str(self.g.v_size()))
 
+    """ This function will draw the graph and run the center algorithm, mark the center node and show the key of the center node in popup """        
+        
     def draw_graph_center(self):
         ga = GraphAlgo(self.g)
         key, dist = ga.centerPoint()
@@ -116,6 +137,8 @@ class Visual:
         self.fig.canvas.draw()
         return key
 
+    """ This function will run shortest path algoritm, show the distance in popup and mark the edges and nodes in the path with special colors """
+    
     def draw_graph_SP(self, id1: int, id2: int):
         ga = GraphAlgo(self.g)
         dist, pathnodes = ga.shortest_path(id1, id2)
@@ -143,6 +166,8 @@ class Visual:
         self.fig.canvas.draw()
         return dist
 
+    """ This function will run shortest path algoritm, show the distance in popup and mark the edges and nodes in the path with special colors """
+    
     def draw_graph_TSP(self, cnodes: List[int]):
         ga = GraphAlgo(self.g)
         pathnodes, dist = ga.TSP(cnodes)
@@ -170,11 +195,17 @@ class Visual:
         self.fig.canvas.draw()
         return dist
 
+    """ This function will get inputs inorder to add new node to the graph, if the inputs are valid and we can add it as new node, we will add it to the graph and draw the
+     new node."""
+    
     def add_node(self, key: int, pos: tuple):
         if self.g.add_node(key, pos):
             self.ax.scatter(pos[0], pos[1], c='b', picker=5, label=str(key))
             self.fig.canvas.draw()
 
+    """ This function will get inputs inorder to add new edge to the graph, if the inputs are valid and we can add it as new edge, we will add it to the graph and draw the
+     new edge."""        
+            
     def add_edge(self, src: int, dest: int, weight: float):
         if self.g.add_edge(src, dest, weight):
             snode = self.g.nodes.get(src, Node)
@@ -185,14 +216,20 @@ class Visual:
                                                                        facecolor='r', shrinkA=0, shrinkB=0))
             self.fig.canvas.draw()
 
+    """ This function will get an input, check if it's a key of a node, and if it is it will remove it from the graph and redraw the graph."""        
+            
     def remove_node(self, key: int):
         if self.g.remove_node(key):
             self.draw_graph()
 
+    """ This function will get an inputs, check if it's a source and destination of an edge, and if it is it will remove it from the graph and redraw the graph."""            
+            
     def remove_edge(self, src: int, dest: int):
         if self.g.remove_edge(src, dest):
             self.draw_graph()
 
+    """ This function supports on_press event, by setting the id of the new node """
+            
     def add_node_by_click(self):
         keys = self.g.get_all_v().keys()
         i = 0
